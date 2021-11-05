f0673f6ea115025573af72911957b345998d426d
slevgolo <mreis94@web.de>
2021-11-05 15:12:55 +0100

    remove crypto tickers because of missing index

--- src/analysing_momentum_trading_strategy.py
+++ src/analysing_momentum_trading_strategy.py
@@ -1,47 +1,22 @@
 # test quantile based trading strategy, see https://www.youtube.com/watch?v=dnrJ4zwCADM
 import datetime as dt
 
+import numpy as np
 import pandas as pd
 import yfinance as yf
-import numpy as np
-
-tickers_yf_crypto = [
-    'BTC-USD',
-    'ETH-USD',
-    'BNB-USD',
-    'ADA-USD',
-    'USDT-USD',
-    'SOL1-USD',
-    'XRP-USD',
-    'HEX-USD',
-    'DOT1-USD',
-    'USDC-USD',
-    'DOGE-USD',
-    'LUNA1-USD',
-    'UNI3-USD',
-    'AVAX-USD',
-    'LINK-USD',
-    'LTC-USD',
-    'BCH-USD',
-    'ALGO-USD',
-    'SHIB-USD',
-    'MATIC-USD',
-    'XLM-USD',
-    'VET-USD',
-    'ICP1-USD',
-    'ATOM1-USD',
-    'FTT1-USD'
-]
 
 # get ticker symbols
-tickers_dji = pd.read_html('https://en.wikipedia.org/wiki/Dow_Jones_Industrial_Average')[1].Symbol.to_list()
-tickers_dax = pd.read_html('https://en.wikipedia.org/wiki/DAX')[3]['Ticker symbol'].to_list()
+# tickers = pd.read_html('https://en.wikipedia.org/wiki/Dow_Jones_Industrial_Average')[1].Symbol.to_list()
+tickers = pd.read_html('https://en.wikipedia.org/wiki/DAX')[3]['Ticker symbol'].to_list()
 
 start = dt.datetime(2018, 1, 31)
 end = dt.datetime(2020, 2, 1)
 
+# index_data = yf.download('^DJI', start=start, end=end, interval='1d')['Adj Close']
+index_data = yf.download('DAX', start=start, end=end, interval='1d')['Adj Close']
+
 # get data from yahoo
-df = yf.download(tickers_dji, start=start.strftime('%Y-%m-%d'), end=end.strftime('%Y-%m-%d'), interval='1d')[
+df = yf.download(tickers, start=start.strftime('%Y-%m-%d'), end=end.strftime('%Y-%m-%d'), interval='1d')[
     'Adj Close']
 
 # calculate monthly returns by cumulating daily returns
@@ -71,6 +46,5 @@ momentumprofit = winner_return.mean() - loser_return.mean()
 print(f'Momentumprofit: {momentumprofit}')
 
 # do benchmark vs whole index
-dji = yf.download('^DJI', start=start, end=end, interval='1d')['Adj Close']
-dji_profit = dji.pct_change().resample('M').agg(lambda x: (x + 1).prod() - 1)[-1]
-print(f'Indexperformance: {dji_profit}')
+index_profit = index_data.pct_change().resample('M').agg(lambda x: (x + 1).prod() - 1)[-1]
+print(f'Indexperformance: {index_profit}')
